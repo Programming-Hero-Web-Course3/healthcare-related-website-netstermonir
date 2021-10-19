afc427fa1ee882836250c46f8cc852b6320150d4
netstermonir <monirhossain6163@gmail.com>
2021-10-19 19:31:47 +0600

    login page height fixed

--- src/Components/Login/Login.js
+++ src/Components/Login/Login.js
@@ -77,66 +77,68 @@ const Login = () => {
     const arrow = <FontAwesomeIcon icon={faArrowRight} />
     const google = <FontAwesomeIcon icon={faGoogle} />
     return (
-        <div className="container" style={{ marginTop: "5rem" }}>
-            <div className="row mx-auto">
-                <div className="col-md-6 mx-auto">
-                    <h2 className="text-center mb-5 text-secondary">Login Now</h2>
-                    <p className="text-danger text-center">{error}</p>
-                    <Form onSubmit={login}>
-                        <Form.Group className="mb-3" id="formBasicemail">
-                            <Form.Label htmlFor="inlineFormInputGroup1" visuallyHidden>
-                                Emaill Address
-                            </Form.Label>
-                            <InputGroup className="mb-2 rounded">
-                                <InputGroup.Text>{envelope}</InputGroup.Text>
-                                <FormControl
-                                    required
-                                    onBlur={getEmail}
-                                    type="email"
-                                    autoComplete="current-email"
-                                    id="email"
-                                    placeholder="Enter Email"
-                                />
-                            </InputGroup>
-                        </Form.Group>
-                        <Form.Group className="mb-3" id="formBasicPassword">
-                            <Form.Label htmlFor="inlineFormInputGroup2" visuallyHidden>
-                                Password
-                            </Form.Label>
-                            <InputGroup className="mb-2 rounded">
-                                <InputGroup.Text>{lock}</InputGroup.Text>
-                                <FormControl
-                                    required
-                                    onBlur={getPassword}
-                                    type="password"
-                                    autoComplete="current-name"
-                                    id="password"
-                                    placeholder="*****"
-                                />
-                            </InputGroup>
-                        </Form.Group>
-                        <div className="d-flex justify-content-between">
-                            <Form.Group className="mb-3" id="formBasicCheckbox">
-                                <Form.Check type="checkbox" label="Remember Me" />
+        <section className="login-height">
+            <div className="container" style={{ marginTop: "5rem" }}>
+                <div className="row mx-auto">
+                    <div className="col-md-6 mx-auto">
+                        <h2 className="text-center mb-5 text-secondary">Login Now</h2>
+                        <p className="text-danger text-center">{error}</p>
+                        <Form onSubmit={login}>
+                            <Form.Group className="mb-3" id="formBasicemail">
+                                <Form.Label htmlFor="inlineFormInputGroup1" visuallyHidden>
+                                    Emaill Address
+                                </Form.Label>
+                                <InputGroup className="mb-2 rounded">
+                                    <InputGroup.Text>{envelope}</InputGroup.Text>
+                                    <FormControl
+                                        required
+                                        onBlur={getEmail}
+                                        type="email"
+                                        autoComplete="current-email"
+                                        id="email"
+                                        placeholder="Enter Email"
+                                    />
+                                </InputGroup>
                             </Form.Group>
-                            <Form.Group className="mb-3" id="formBasicCheckbox">
-                                <Button onClick={handlePasswordreset} variant="outline-primary">Forgot Password</Button>
+                            <Form.Group className="mb-3" id="formBasicPassword">
+                                <Form.Label htmlFor="inlineFormInputGroup2" visuallyHidden>
+                                    Password
+                                </Form.Label>
+                                <InputGroup className="mb-2 rounded">
+                                    <InputGroup.Text>{lock}</InputGroup.Text>
+                                    <FormControl
+                                        required
+                                        onBlur={getPassword}
+                                        type="password"
+                                        autoComplete="current-name"
+                                        id="password"
+                                        placeholder="*****"
+                                    />
+                                </InputGroup>
                             </Form.Group>
+                            <div className="d-flex justify-content-between">
+                                <Form.Group className="mb-3" id="formBasicCheckbox">
+                                    <Form.Check type="checkbox" label="Remember Me" />
+                                </Form.Group>
+                                <Form.Group className="mb-3" id="formBasicCheckbox">
+                                    <Button onClick={handlePasswordreset} variant="outline-primary">Forgot Password</Button>
+                                </Form.Group>
+                            </div>
+                            <Button variant="info" className="w-100 fw-bold" type="submit">
+                                {arrow} Login
+                            </Button>
+                        </Form>
+                        <NavLink className="text-decoration-none" to="/register">
+                            <p className="text-center mt-3 text-dark fw-bolder">New Member? Please Create An Account!</p>
+                        </NavLink>
+                        <div className="mt-3 text-center fs-4 fw-bolder text-muted">---Or---</div>
+                        <div className="mt-3 mb-3 text-center fs-4 fw-bolder" >
+                            <Button onClick={handleGoogleLogin} variant="info"> {google}</Button>
                         </div>
-                        <Button variant="info" className="w-100 fw-bold" type="submit">
-                            {arrow} Login
-                        </Button>
-                    </Form>
-                    <NavLink className="text-decoration-none" to="/register">
-                        <p className="text-center mt-3 text-dark fw-bolder">New Member? Please Create An Account!</p>
-                    </NavLink>
-                    <div className="mt-3 text-center fs-4 fw-bolder text-muted">---Or---</div>
-                    <div className="mt-3 mb-3 text-center fs-4 fw-bolder" >
-                        <Button onClick={handleGoogleLogin} variant="info"> {google}</Button>
                     </div>
                 </div>
-            </div>
-        </div >
+            </div >
+        </section>
     );
 };
 
