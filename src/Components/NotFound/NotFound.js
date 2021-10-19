6edaabb21b7513c8de75bd0c64f5bfb7f9aa938c
netstermonir <monirhossain6163@gmail.com>
2021-10-19 18:17:06 +0600

    all fixed

--- src/Components/NotFound/NotFound.js
+++ src/Components/NotFound/NotFound.js
@@ -1,21 +1,30 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import { Button } from 'react-bootstrap';
+import './NotFound.css';
 import notFound from '../../image/404/404.png';
 
 const NotFound = () => {
     return (
         <>
-            <div className="mt-5">
-                <img src={notFound} alt="Page Not Found" />
-            </div>
-            <div className="mt-5">
-                <Link to="/">
-                    <Button variant="info" className="fs-5 fw-bolder">
-                        Go Back Home
-                    </Button>
-                </Link>
-            </div>
+            <section className="notfound">
+                <div className="container">
+                    <div className="row">
+                        <div className="col-md-6 mx-auto">
+                            <div className="mt-5">
+                                <img src={notFound} alt="Page Not Found" />
+                            </div>
+                            <div className="mt-5 mx-auto text-center">
+                                <Link to="/">
+                                    <Button variant="info" className="fs-5 fw-bolder">
+                                        Go Back Home
+                                    </Button>
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </>
     );
 };
